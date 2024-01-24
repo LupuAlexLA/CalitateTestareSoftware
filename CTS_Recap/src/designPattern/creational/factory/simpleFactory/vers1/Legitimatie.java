dbd5539d52a01cf474386d8fd3cbedc03cd37e22
Lupu Alex <alex@nivasoftware.ro>
2024-01-24 19:55:48 +0200

    nu stiu ce saq mai fac cu ele

--- CTS_Recap/src/designPattern/creational/factory/simpleFactory/vers1/Legitimatie.java
+++
@@ -1,5 +0,0 @@
-package designPattern.creational.factory.simpleFactory.vers1;
-
-public interface Legitimatie {
-    public void descriere();
-}
--- CTS_Recap/src/designPattern/creational/factory/simpleFactory/vers1/LegitimatieDirector.java
+++
@@ -1,8 +0,0 @@
-package designPattern.creational.factory.simpleFactory.vers1;
-
-public class LegitimatieDirector implements Legitimatie{
-    @Override
-    public void descriere() {
-        System.out.println("Legitimatie! Este Director la Firma X");
-    }
-}
